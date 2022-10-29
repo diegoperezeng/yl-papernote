eb5695d5bda731c4e1f8edaf30297474d0e461f8
diegoperezeng <diegoperez.eng@gmail.com>
2022-10-29 19:00:30 -0300

    Springboot base project - Dependency Spring web - Dependency MongoDB

---
+++ src/main/java/com/yellowleaf/ylpapernote/YlPapernoteApplication.java
@@ -0,0 +1,13 @@
+package com.yellowleaf.ylpapernote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlPapernoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YlPapernoteApplication.class, args);
+	}
+
+}
